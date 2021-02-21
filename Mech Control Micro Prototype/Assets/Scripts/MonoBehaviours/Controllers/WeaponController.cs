0164d711429d2f29fe6a940846399c56df5cde1e
Andrew Allbright <aallbrig@gmail.com>
2021-02-21 12:47:43 -0500

    Player aim implemented

---
+++ Mech Control Micro Prototype/Assets/Scripts/MonoBehaviours/Controllers/WeaponController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace MonoBehaviours.Controllers
+{
+    public class WeaponController : MonoBehaviour
+    {
+        public GameObject mainCamera;
+        public GameObject aimCamera;
+
+        private void Update()
+        {
+            // Left click fires
+            // Right click aims
+            if (Input.GetMouseButtonDown(1))
+            {
+                mainCamera.SetActive(false);
+                aimCamera.SetActive(true);
+            } else if (Input.GetMouseButtonUp(1))
+            {
+                mainCamera.SetActive(true);
+                aimCamera.SetActive(false);
+            }
+        }
+    }
+}
